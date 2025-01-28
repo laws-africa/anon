4aefe25e415b49e6c21e1c3c6a11c884a165a14f
Greg Kempe <greg@kempe.net>
2025-01-28 07:21:00 +0200

    adjust base dir for build

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  base: '/anon/',
   build: {
     outDir: './docs',
     emptyOutDir: true,
